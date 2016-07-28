808f46099a28e29f8b86b8f1495d44138344cb0b
crazybeeliuzhe <zheliu@outlook.com>
2016-07-28 13:21:33 +0200

    PdfSpliter

---
+++ PdfSpliter/PdfSpliter/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PdfSpliter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileselect = new System.Windows.Forms.Button();
+            this.inputfilepath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputinformation = new System.Windows.Forms.TextBox();
+            this.run = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // fileselect
+            // 
+            this.fileselect.Location = new System.Drawing.Point(13, 27);
+            this.fileselect.Name = "fileselect";
+            this.fileselect.Size = new System.Drawing.Size(259, 23);
+            this.fileselect.TabIndex = 0;
+            this.fileselect.Text = "Select File";
+            this.fileselect.UseVisualStyleBackColor = true;
+            this.fileselect.Click += new System.EventHandler(this.fileselect_Click);
+            // 
+            // inputfilepath
+            // 
+            this.inputfilepath.Location = new System.Drawing.Point(13, 72);
+            this.inputfilepath.Name = "inputfilepath";
+            this.inputfilepath.Size = new System.Drawing.Size(259, 20);
+            this.inputfilepath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // outputinformation
+            // 
+            this.outputinformation.BackColor = System.Drawing.SystemColors.Control;
+            this.outputinformation.Location = new System.Drawing.Point(138, 162);
+            this.outputinformation.Name = "outputinformation";
+            this.outputinformation.Size = new System.Drawing.Size(134, 20);
+            this.outputinformation.TabIndex = 4;
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(177, 217);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(95, 47);
+            this.run.TabIndex = 5;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 116);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 276);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.outputinformation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputfilepath);
+            this.Controls.Add(this.fileselect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileselect;
+        private System.Windows.Forms.TextBox inputfilepath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox outputinformation;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PdfSpliter/PdfSpliter/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSpliter.Properties;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PdfSpliter
+{
+    public partial class Form1 : Form
+    {
+        private static string outputfolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Resources.Form1_Form1_PDF_spliter;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void fileselect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.InitialDirectory = Environment.CurrentDirectory;
+            fdlg.Filter = "Pdf Files (.pdf)|*.pdf|All Files (*.*)|*.*";
+            fdlg.RestoreDirectory = true;
+            DialogResult result = fdlg.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = fdlg.FileName;
+                try
+                {
+                    inputfilepath.Text = file;
+                }
+                catch (IOException)
+                {
+                    outputinformation.Text = Resources.Form1_fileselect_Click_file_io_error;
+                }
+            }
+        }
+
+        private async void run_Click(object sender, EventArgs e)
+        {
+            outputinformation.Text = "";
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            var progress = new Progress<int>(v =>
+            {
+                // This lambda is executed in context of UI thread,
+                // so it can safely update form controls
+                progressBar1.Value = v;
+            });
+
+            await Task.Run(() => OutPutPdfJob(progress));
+            progressBar1.Text = "Succeed";
+            outputinformation.Text = "Succeed";
+            Showoutput(outputfolder);
+        }
+
+        private void OutPutPdfJob(IProgress<int> progress)
+        {
+            // Get a fresh copy of the sample PDF file
+            var filename = inputfilepath.Text;
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                outputinformation.Text = "input file name is empty";
+                return;
+            }
+            // Open the file
+
+            PdfDocument inputDocument = PdfReader.Open(filename, PdfDocumentOpenMode.Import);
+            string name = Path.GetFileNameWithoutExtension(filename);
+            string outFolder = Path.GetDirectoryName(filename);
+            if (outFolder != null)
+            {
+                var outputFolder = Path.Combine(outFolder, name);
+                if (!Directory.Exists(outputFolder))
+                {
+                    Directory.CreateDirectory(outputFolder);
+                }
+
+                try
+                {
+                    var total = inputDocument.PageCount;
+
+                    for (int idx = 0; idx < total; idx++)
+                    {
+                        // Create new document
+                        PdfDocument outputDocument = new PdfDocument();
+                        outputDocument.Options.CompressContentStreams = true;
+                        // Add the page and save it
+                        outputDocument.AddPage(inputDocument.Pages[idx]);
+                        var outputfilename = String.Format("{0} - Page {1}_tempfile.pdf", name, idx + 1);
+                        outputfilename = Path.Combine(outputFolder, outputfilename);
+                        outputDocument.Save(outputfilename);
+                        if (progress != null)
+                            progress.Report((idx + 1)*100/total);
+                    }
+                }
+                catch (Exception)
+                {
+                    outputinformation.Text = "Exception throws";
+                    throw;
+                }
+
+                outputfolder = outputFolder;
+            }
+        }
+
+        private static void Showoutput(string outputFolder)
+        {
+            OpenFileDialog outputDialog = new OpenFileDialog();
+            outputDialog.InitialDirectory = outputFolder;
+            outputDialog.ShowDialog();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
